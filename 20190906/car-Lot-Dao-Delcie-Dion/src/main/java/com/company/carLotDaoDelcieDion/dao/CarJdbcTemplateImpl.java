8a81331093d270fe0fcaf3ea0405bbd03f5a1c4c
Asteria <dcdion22@gmail.com>
2019-09-05 20:26:23 -0400

    20190906 carLotDao

--- 20190906/car-Lot-Dao-Delcie-Dion/src/main/java/com/company/carLotDaoDelcieDion/dao/CarJdbcTemplateImpl.java
+++ 20190906/car-Lot-Dao-Delcie-Dion/src/main/java/com/company/carLotDaoDelcieDion/dao/CarJdbcTemplateImpl.java
@@ -1,11 +1,46 @@
 package com.company.carLotDaoDelcieDion.dao;
 
 import com.company.carLotDaoDelcieDion.model.Car;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+@Repository
 public class CarJdbcTemplateImpl implements CarDao {
+
+    private static final String INSERT_CAR_SQL =
+            "INSERT INTO CAR (MAKE, MODEL, YEAR, COLOR) VALUES (?,?,?,?)";
+
+    private static final String SELECT_CAR_SQL=
+            "select * from car where id = ?";
+
+    private static final String SELECT_ALL_CARS_SQL=
+            "select * from car";
+
+    private static final String DELETE_CAR_SQL=
+            "delete from car where id = ?";
+
+    private static final String UPDATE_CAR_SQL=
+            "update car set make = ?, model = ?, year = ?, color = ? where id = ?";
+
+    private static final String SELECT_CAR_BY_MAKE_SQL=
+            "select * from car where make = ?";
+
+    private static final String SELECT_CAR_BY_COLOR_SQL=
+            "select * from car where color = ?";
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public CarJdbcTemplateImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     /**
      * Getting one car for the given id
@@ -15,7 +50,12 @@ public class CarJdbcTemplateImpl implements CarDao {
      */
     @Override
     public Car getcar(int id) {
-        return null;
+        try {
+            return jdbcTemplate.queryForObject(SELECT_CAR_SQL, this::mapRowToCar, id);
+        } catch (EmptyResultDataAccessException e) {
+            // if nothing is returned just catch the exception and return null;
+            return null;
+        }
     }
 
     /**
@@ -25,7 +65,7 @@ public class CarJdbcTemplateImpl implements CarDao {
      */
     @Override
     public List<Car> getAllCars() {
-        return null;
+        return jdbcTemplate.query(SELECT_ALL_CARS_SQL, this::mapRowToCar);
     }
 
     /**
@@ -37,7 +77,14 @@ public class CarJdbcTemplateImpl implements CarDao {
     @Override
     @Transactional
     public Car addCar(Car car) {
-        return null;
+        jdbcTemplate.update(INSERT_CAR_SQL,
+                car.getMake(),
+                car.getModel(),
+                car.getYear(),
+                car.getColor());
+        int id = jdbcTemplate.queryForObject("select last_insert_id()", Integer.class);
+        car.setId(id);
+        return car;
     }
 
     /**
@@ -47,7 +94,12 @@ public class CarJdbcTemplateImpl implements CarDao {
      */
     @Override
     public void updateCar(Car car) {
-
+        jdbcTemplate.update(UPDATE_CAR_SQL,
+                car.getMake(),
+                car.getModel(),
+                car.getYear(),
+                car.getColor(),
+                car.getId());
     }
 
     /**
@@ -58,7 +110,7 @@ public class CarJdbcTemplateImpl implements CarDao {
      */
     @Override
     public void deleteCar(int id) {
-
+        jdbcTemplate.update(DELETE_CAR_SQL, id);
     }
 
     /**
@@ -69,7 +121,7 @@ public class CarJdbcTemplateImpl implements CarDao {
      */
     @Override
     public List<Car> getCarByMake(String make) {
-        return null;
+        return jdbcTemplate.query(SELECT_CAR_BY_MAKE_SQL, this::mapRowToCar, make);
     }
 
     /**
@@ -80,6 +132,24 @@ public class CarJdbcTemplateImpl implements CarDao {
      */
     @Override
     public List<Car> getCarByColor(String color) {
-        return null;
+        return jdbcTemplate.query(SELECT_CAR_BY_COLOR_SQL, this::mapRowToCar, color);
     }
+
+    /**
+     * Maps motorcycle table row to Motorcycle object
+     * @param rs
+     * @param rowNum
+     * @return
+     * @throws SQLException
+     */
+    private Car mapRowToCar(ResultSet rs, int rowNum) throws SQLException {
+        Car car = new Car();
+        car.setId(rs.getInt("id"));
+        car.setMake(rs.getString("make"));
+        car.setModel(rs.getString("model"));
+        car.setYear(rs.getString("year"));
+        car.setColor(rs.getString("color"));
+        return car;
+    }
+
 }
--- 20190906/car-Lot-Dao-Delcie-Dion/src/test/java/com/company/carLotDaoDelcieDion/dao/CarDaoTest.java
+++ 20190906/car-Lot-Dao-Delcie-Dion/src/test/java/com/company/carLotDaoDelcieDion/dao/CarDaoTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -27,32 +28,116 @@ public class CarDaoTest {
     }
 
     @Test
-    public void getcar() {
+    public void addGetDeleteCar() {
         Car car = new Car();
+        car.setMake("Ford");
+        car.setModel("Mustang");
+        car.setYear("2012");
+        car.setColor("Blue");
+        car = dao.addCar(car);
 
+        Car car2 = dao.getcar(car.getId());
+
+        assertEquals(car, car2);
+
+        dao.deleteCar(car.getId());
+        car2 = dao.getcar(car.getId());
+        assertNull(car2);
     }
 
     @Test
     public void getAllCars() {
-    }
+        Car car = new Car();
+        car.setMake("Ford");
+        car.setModel("Mustang");
+        car.setYear("2012");
+        car.setColor("Blue");
+        car = dao.addCar(car);
 
-    @Test
-    public void addCar() {
+        Car car2 = new Car();
+        car2.setMake("Ford");
+        car2.setModel("Fiesta");
+        car2.setYear("2030");
+        car2.setColor("Blue");
+        car2 = dao.addCar(car2);
+
+        List<Car> carList = new ArrayList<>();
+        carList.add(car);
+        carList.add(car2);
+
+        List<Car> carList2 = dao.getAllCars();
+
+        assertEquals(carList.size(), carList2.size());
+
+
     }
 
     @Test
     public void updateCar() {
-    }
+        Car car = new Car();
+        car.setMake("Ford");
+        car.setModel("Mustang");
+        car.setYear("2012");
+        car.setColor("Blue");
+        car = dao.addCar(car);
 
-    @Test
-    public void deleteCar() {
+        car.setMake("NEW MAKE");
+        car.setModel("Fiesta");
+        car.setYear("1000");
+        car.setColor("NEW COLOR");
+        dao.addCar(car);
+
+        Car car2 = dao.getcar(car.getId());
+        assertEquals(car, car2);
     }
 
     @Test
     public void getCarByMake() {
+        Car car = new Car();
+        car.setMake("Honda");
+        car.setModel("Africa Twin");
+        car.setYear("2019");
+        car.setColor("black");
+        dao.addCar(car);
+
+        car.setMake("Chevy");
+        car.setModel("Camaro");
+        car.setYear("2019");
+        car.setColor("black");
+        dao.addCar(car);
+
+        car.setMake("Chevy");
+        car.setModel("Camaro");
+        car.setYear("1976");
+        car.setColor("blue");
+        dao.addCar(car);
+
+        List<Car> carList = dao.getCarByMake("Chevy");
+        assertEquals(2, carList.size());
     }
 
     @Test
     public void getCarByColor() {
+        Car car = new Car();
+        car.setMake("Honda");
+        car.setModel("Africa Twin");
+        car.setYear("2019");
+        car.setColor("black");
+        dao.addCar(car);
+
+        car.setMake("Chevy");
+        car.setModel("Camaro");
+        car.setYear("2019");
+        car.setColor("black");
+        dao.addCar(car);
+
+        car.setMake("Chevy");
+        car.setModel("Camaro");
+        car.setYear("1976");
+        car.setColor("blue");
+        dao.addCar(car);
+
+        List<Car> carList = dao.getCarByColor("black");
+        assertEquals(2, carList.size());
     }
 }
